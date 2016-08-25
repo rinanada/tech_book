46ccead63c0ab612c27466cf2190c54f83eec693
灘　梨菜 <nada_rina@nadarina-no-MacBook-Air.local>
2016-08-25 18:29:36 +0900

    exhibit,userモデル作成

---
+++ app/models/exibit.rb
@@ -0,0 +1,2 @@
+class Exibit < ActiveRecord::Base
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,41 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160825092331) do
+
+  create_table "exibits", force: :cascade do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "title",       limit: 255
+    t.string   "sub_title",   limit: 255
+    t.integer  "price",       limit: 4
+    t.integer  "state",       limit: 4
+    t.integer  "user_id",     limit: 4
+    t.integer  "category_id", limit: 4
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  limit: 255,   default: "", null: false
+    t.string   "encrypted_password",     limit: 255,   default: "", null: false
+    t.string   "reset_password_token",   limit: 255
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          limit: 4,     default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
+    t.string   "username",               limit: 255
+    t.text     "pro_img",                limit: 65535
+    t.text     "image_cache",            limit: 65535
+    t.text     "introduction",           limit: 65535
+    t.integer  "callnumber",             limit: 4
+    t.integer  "postal_code",            limit: 4
+    t.datetime "created_at",                                        null: false
+    t.datetime "updated_at",                                        null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
