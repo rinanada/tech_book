0f17b98379239fa1fab5fe0ef1c724854abd5cbb
灘　梨菜 <nada_rina@nadarina-no-MacBook-Air.local>
2016-08-25 18:55:18 +0900

    未定義のメソッドを、ログインフォームを組み込みたいViewのhelperに組み込んだ。 新規登録フォームでdbにデータを入れることに成功。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    # sign_inのときに、group_keyも許可する
+      devise_parameter_sanitizer.permit(:sign_in, keys:[:username, :email])
+    # sign_upのときに、group_keyも許可する
+      devise_parameter_sanitizer.permit(:sign_up, keys:[:username, :email])
+    #account_updateのときに、group_keyも許可する
+      devise_parameter_sanitizer.permit(:account_update, keys:[:username])
+  end
 end
