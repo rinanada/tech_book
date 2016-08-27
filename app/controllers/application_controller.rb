245a7b271ce96106ce22e294dd46be46db509837
灘　梨菜 <nada_rina@nadarina-no-MacBook-Air.local>
2016-08-27 16:31:54 +0900

    deviseのカスタマイズ手直し

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,12 +16,12 @@ class ApplicationController < ActionController::Base
   end
 
 
-  def after_sign_in_path_for(resource)
-    binding.pry
-    root_path
-  end
+  # def after_sign_in_path_for(resource)
+  #   binding.pry
+  #   root_path
+  # end
 
-  def after_sign_out_path_for(resource)
-    admin_root_path
-  end
+  # def after_sign_out_path_for(resource)
+  #   admin_root_path
+  # end
 end
