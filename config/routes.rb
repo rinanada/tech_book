1c488a27ef3208b2fe34372e8fa758f3794fecef
灘　梨菜 <nada_rina@nadarina-no-MacBook-Air.local>
2016-08-28 21:14:49 +0900

    profiles/editの作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :sessions => 'users/sessions'
 }
   resources :exhibits, only: [:show]
-  resources :users, only: [:show]
+  resources :users, only: [:show, :update]
   resources :buyings, only: [:show, :new]
+  resources :profiles, only: [:edit]
 
 end
