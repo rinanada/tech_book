d4bcdbbe681ee3ce78d5d9fbd58c851c6ce98c43
灘　梨菜 <nada_rina@nadarina-no-MacBook-Air.local>
2016-08-30 15:54:36 +0900

    address model migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160829100305) do
+ActiveRecord::Schema.define(version: 20160830032228) do
+
+  create_table "addresses", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "buyings", force: :cascade do |t|
     t.datetime "created_at"
