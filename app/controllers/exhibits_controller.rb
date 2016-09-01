5ddf666cabb6f1a8e5c39e6f5992a6d8f328cf88
灘　梨菜 <nada_rina@nadarina-no-MacBook-Air.local>
2016-09-01 12:02:46 +0900

    宛先についてのカラムが空なら「名無しさん」と表示、あるならトップページにfull nameで表示

--- app/controllers/exhibits_controller.rb
+++ app/controllers/exhibits_controller.rb
@@ -1,8 +1,8 @@
 class ExhibitsController < ApplicationController
   def index
     @user_detail = UserDetail.find(1)
-    @mastname = @user_detail.try(:firstname) || '名無し'
-    @mastname += @user_detail.try(:lastname) || '名無し'
+    @mastname = @user_detail.try(:lastname) || '名無し'
+    @mastname += @user_detail.try(:firstname) || '名無し'
     @exhibits = Exhibit.includes(:user)
   end
 
