5a1c36cef8293f9169c58041dc0886a5098701ce
灘　梨菜 <nada_rina@nadarina-no-MacBook-Air.local>
2016-09-01 12:03:59 +0900

    現在ログイン中のユーザーに紐付いた詳細情報をユーザーマイページにおいてアップデート可能に

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:edit, :update]
   before_action :authenticate_user!, only: [:edit, :update]
 
   def edit
@@ -12,19 +12,28 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user.update(user_params)
-    binding.pry
-    redirect_to :edit_user
+    # if @user_detail.changed then
+      @user_detail.update(user_detail_params)
+      redirect_to :edit_user
+    # else
+    #   @user.update(user_params)
+    # remote ture
   end
 
   private
 
   def set_user
-    @user = User.find(params[:id])
+    @user = User.find(current_user)
+    @user_detail = UserDetail.find_by(user_id: @user.id)
+
   end
 
   def user_params
-    params.require(:user).permit(:lastname, :firstname, :postalcode, :place, :email)
+    params.require(:user).permit(:email)
+  end
+
+  def user_detail_params
+    params.require(:user_detail).permit(:lastname, :firstname, :postalcode, :callnumber, :place)
   end
 end
 
