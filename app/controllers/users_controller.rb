6ccd59affb06b268e4478fbcfd0e9bb1cb7ad71c
灘　梨菜 <nada_rina@nadarina-no-MacBook-Air.local>
2016-09-02 13:19:00 +0900

    @user_detailをネストして二回分の入力フォーム作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,41 +1,48 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:edit, :update]
+  before_action :set_user, only: [:update, :destory]
   before_action :authenticate_user!, only: [:edit, :update]
 
   def edit
+    @user = User.new
+    2.times { @user.user_details.build }
+    # @user_details = UserDetail.new
   end
 
-  def index
+
+  def create
+    @user = User.new(user_params)
+      # @user = User.find(current_user)
+      if @user.save
+      binding.pry
+      redirect_to :edit_user, notice: 'New profile was successfully created'
+      else
+      redirect_to :edit_user, alert: 'New prototype was unsuccessfully created'
+      end
   end
 
-  def show
+  def destroy
   end
 
   def update
-    # if @user_detail.changed
-      @user_detail.update(user_detail_params)
-      redirect_to :edit_user
-    # else
-    #   @user.update(user_params)
-    # remote ture
+    if @user.update(user_params)
+      redirect_to :edit_user, notice: 'Your profile was successfully updated'
+    else
+      flash.now[:alert] = 'Your profile was unsuccessfully updated'
+    end
   end
 
   private
 
   def set_user
-    @user = User.find(current_user)
-    @user_detail = UserDetail.find_by(user_id: @user.id)
-  end
-
-
-  def user_detail_params
-    params.require(:user_detail).permit(:lastname, :firstname, :postalcode, :callnumber, :place)
+      @user = User.find(params[:id])
+      # @user_detail = UserDetail.find_by(user_id: @user.id)
   end
 
 
   def user_params
-    params.require(:user).permit(:email)
+    params.require(:user).permit(:id, :email, user_details_attributes: [:lastname, :firstname, :postalcode, :callnumber, :place])
   end
+
 
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root "exhibits#index"
   devise_for :users, :controllers => { sessions: 'users/sessions' }
   resources :exhibits, only: [:show]
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:edit, :update, :create]
   resources :buyings, only: [:show, :new, :update, :create]
   resources :profiles, only: [:show]
 
