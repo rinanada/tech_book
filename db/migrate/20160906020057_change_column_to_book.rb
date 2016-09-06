c94300cdd038456748fef7a9da3317ee758747c4
灘　梨菜 <nada_rina@nadarina-no-MacBook-Air.local>
2016-09-06 17:46:34 +0900

    model設計見直し

--- db/migrate/20160906020057_change_column_to_book.rb
+++
@@ -1,8 +0,0 @@
-class ChangeColumnToBook < ActiveRecord::Migration
-  def change
-    remove_column :books, :order_user_id, :integer
-    remove_column :books, :likes_count, :integer
-    remove_column :books, :discription, :text
-    add_column :books, :description, :text
-  end
-end
