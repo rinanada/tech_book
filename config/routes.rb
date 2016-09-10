42131ee90ee8be6d94495ea1f60a33d3d44739cd
灘　梨菜 <nada_rina@nadarina-no-MacBook-Air.local>
2016-09-10 16:30:43 +0900

    OAuthのコールバック用のルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   root "books#index"
-  devise_for :users, :controllers => { sessions: 'users/sessions' }
+  devise_for :users, controllers: { sessions: 'users/sessions', omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :user_details, only: [:new, :create, :edit, :update]
   resources :profiles, only: [:show]
 
