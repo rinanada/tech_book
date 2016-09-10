ee1761f1e903dcdd30c7260eea7b10000eaef641
灘　梨菜 <nada_rina@nadarina-no-MacBook-Air.local>
2016-09-10 16:32:37 +0900

    プロバイダの各キーを設定

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -244,6 +244,8 @@ Devise.setup do |config|
   # The default HTTP method used to sign out a resource. Default is :delete.
   config.sign_out_via = :delete
 
+  config.omniauth :facebook, '185055705253222', '12a26974c3eda87e09772feeb8866a47'
+
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
