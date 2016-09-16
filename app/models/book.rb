5888659bb3a9c89f2d4f3bdb4bb00a10e7307063
灘　梨菜 <nada_rina@nadarina-no-MacBook-Air.local>
2016-09-16 20:49:10 +0900

    add scope

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,6 +4,9 @@ class Book < ActiveRecord::Base
   has_one :order
   has_many :likes
   has_many :categories
+  validates :title, presence: true
+  validates :title, length: { maximum: 15}
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
   mount_uploader :content, BookImageUploader
   acts_as_taggable_on :categories
   def tax
@@ -13,4 +16,13 @@ class Book < ActiveRecord::Base
   def total_price
     (price + 200) * 1.08.round
   end
+
+  scope :costry, -> { where("price > ?", 3000) }
+  # Book.costry
+  scope :writen_about, -> { where("name like ?", "%#{theme}")}
+  # Book.costry.writen_about("Java")
+  # =>「高価」で「Java」について書かれた本を探している
+
+  # default_scope -> { order("created_at desc") }
+  # => indexで代用
 end
