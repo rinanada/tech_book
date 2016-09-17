504e63baf2ee08c8b190c570b31b69a2a9396394
灘　梨菜 <nada_rina@nadarina-no-MacBook-Air.local>
2016-09-17 11:07:32 +0900

    irb(main):051:0> Book.all.each do |book| irb(main):052:1* book.update(state: 2) irb(main):053:1> end   Book Load (0.3ms)  SELECT `books`.* FROM `books`    (0.2ms)  BEGIN   SQL (0.4ms)  UPDATE `books` SET `state` = 2, `updated_at` = '2016-09-17 02:05:12' WHERE `books`.`id` = 1    (2.1ms)  COMMIT    (0.2ms)  BEGIN   SQL (0.4ms)  UPDATE `books` SET `state` = 2, `updated_at` = '2016-09-17 02:05:12' WHERE `books`.`id` = 2    (2.1ms)  COMMIT    (0.8ms)  BEGIN   SQL (0.3ms)  UPDATE `books` SET `state` = 2, `updated_at` = '2016-09-17 02:05:12' WHERE `books`.`id` = 3    (2.2ms)  COMMIT    (0.4ms)  BEGIN   SQL (0.3ms)  UPDATE `books` SET `state` = 2, `updated_at` = '2016-09-17 02:05:12' WHERE `books`.`id` = 4    (2.3ms)  COMMIT    (0.1ms)  BEGIN   SQL (0.4ms)  UPDATE `books` SET `state` = 2, `updated_at` = '2016-09-17 02:05:12' WHERE `books`.`id` = 5    (2.4ms)  COMMIT    (0.2ms)  BEGIN   SQL (0.4ms)  UPDATE `books` SET `state` = 2, `updated_at` = '2016-09-17 02:05:12' WHERE `books`.`id` = 6    (5.2ms)  COMMIT    (0.2ms)  BEGIN   SQL (0.3ms)  UPDATE `books` SET `state` = 2, `updated_at` = '2016-09-17 02:05:12' WHERE `books`.`id` = 7    (2.1ms)  COMMIT
    resolut validation error while launch rails c

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ActiveRecord::Base
+  enum state: {very_good: 0, good: 1, so_so: 2, bad: 3}
+
   belongs_to :o_user, :class_name => 'User', :foreign_key => 'o_user_id'
   belongs_to :e_user, :class_name => 'User', :foreign_key => 'e_user_id'
   has_one :order
@@ -7,8 +9,10 @@ class Book < ActiveRecord::Base
 
   before_validation :add_js
   validates :title, presence: true
-  validates :title, length: { maximum: 15}
+  validates :title, length: { maximum: 60 }
   validates :price, numericality: { greater_than_or_equal_to: 0 }
+  validates :state, presence: true
+
   mount_uploader :content, BookImageUploader
   acts_as_taggable_on :categories
   def tax
@@ -35,7 +39,6 @@ class Book < ActiveRecord::Base
   end
 
   after_destroy do |book|
-    binding.pry
     Rails.logger.info "Book is deleted: #{book.attributes.inspect}"
   end
 end
