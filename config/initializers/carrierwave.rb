d21331de8288200460e969bac7c733b50375980f
灘　梨菜 <nada_rina@nadarina-no-MacBook-Air.local>
2016-10-14 22:24:24 +0900

    s3はcapistranoが適用されてから

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: ENV['ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
+    # aws_access_key_id: ENV['ACCESS_KEY_ID'],
+    # aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
     region: 'us-east-1'
   }
 
