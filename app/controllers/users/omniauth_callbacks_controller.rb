14d23bae074490a3edea5a8d1034c6bd97db907a
灘　梨菜 <nada_rina@nadarina-no-MacBook-Air.local>
2016-10-15 09:11:17 +0900

    Facebookとtwitterのcallback時のメソッド分離

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,28 +1,24 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  def facebook
-    callback_from :facebook
+    def facebook
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+    if @user.persisted?
+      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+      set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+    else
+      # session["devise.facebook_data"] = request.env["omniauth.auth"]
+      redirect_to root_path, flash: { error: "Facebookでの認証に失敗しました" }
+    end
   end
+
 
   def twitter
-  end
-
-  # def twitter
-  #   callback_from :twitter
-  # end
-
-  private
-
-  def callback_from(provider)
-    provider = provider.to_s
-
-    @user = User.find_for_oauth(request.env['omniauth.auth'])
-
+    set_dummy_email
+    @user = User.from_omniauth(request.env["omniauth.auth"])
     if @user.persisted?
-      flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
-      sign_in_and_redirect @user, event: :authentication
+      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+      set_flash_message(:notice, :success, :kind => "Twitter") if is_navigational_format?
     else
-      session["devise.#{provider}_data"] = request.env['omniauth.auth']
-      redirect_to new_user_registration_url
+      redirect_to root_path, flash: { error: "Twitterでの認証に失敗しました" }
     end
   end
 end
