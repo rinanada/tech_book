cc84454fa2ac47383f2b16037d3ea7bc3d47560c
灘　梨菜 <nada_rina@nadarina-no-MacBook-Air.local>
2016-10-15 14:42:31 +0900

    deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,11 +17,38 @@ set :log_level, :debug
 
 
 namespace :deploy do
-  after :publishing, :restart
+  desc 'Create database'
+  task :db_create do
+    on roles(:db) do |host|
+      with rails_env: fetch(:rails_env) do
+        within current_path do
+          execute :bundle, :exec, :rake, 'db:create'
+        end
+      end
+    end
+  end
+
+  desc 'Run seed'
+  task :seed do
+    on roles(:app) do
+      with rails_env: fetch(:rails_env) do
+        within current_path do
+          execute :bundle, :exec, :rake, 'db:seed'
+        end
+      end
+    end
+  end
+
 
   desc "Restart application"
   task :restart do
     invoke "unicorn:restart"
+  end
+
+  after :publishing, :restart
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+    end
   end
 end
 
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,7 +3,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     Rails.application.secrets.twitter_api_key,
     Rails.application.secrets.twitter_api_secret
   provider :facebook,
-    Rails.application.secrets.facebook_id,
+    Rails.application.secrets.facebook_api_key,
     Rails.application.secrets.facebook_secret_key, scope: 'email,status_update,publish_actions'
 end
 
