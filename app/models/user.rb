7335bc182e991be9b0cab769c823d97c392a0c71
灘　梨菜 <nada_rina@nadarina-no-MacBook-Air.local>
2016-10-16 15:11:06 +0900

    ・user/modelでoauth認証時もメールアドレスを保存するためdummy_email作成 ・omniauth_controllerではコールバックをprivateに

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,15 +13,25 @@ class User < ActiveRecord::Base
 
   accepts_nested_attributes_for :user_details, allow_destroy: true
   devise :omniauthable, omniauth_providers: [:facebook, :twitter]
-  
 
-def self.from_omniauth(auth)
-  where(auth.slice(:provider, :uid)).first_or_create do |user|
-    user.email = auth.info.email
-    user.password = Devise.friendly_token[0,20]
+  def self.from_omniauth(auth)
+    user = User.where(uid: auth.uid, provider: auth.provider).first
+
+    unless user
+      user = User.create(
+        uid:      auth.uid,
+        provider: auth.provider,
+        email:    User.dummy_email(auth),
+        password: Devise.friendly_token[0, 20]
+      )
+    end
+
+    user
   end
-end
 
+  private
 
-
+  def self.dummy_email(auth)
+    "#{auth.uid}-#{auth.provider}@example.com"
+  end
 end
