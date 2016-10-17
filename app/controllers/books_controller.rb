ab8ae699982cec572a18e735b02dba39e3399915
灘　梨菜 <nada_rina@nadarina-no-MacBook-Air.local>
2016-10-18 00:51:27 +0900

    books/showでの関連書籍の表示

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -30,6 +30,8 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
     @user_detail = UserDetail.find_by(id: @book.user_detail.id)
+    @related_books = Book.tagged_with(@book.category_list, any: true).uniq.where.not(id: @book.id)
+    # max 3s冊まで
   end
 
   private
