a697cbb957eb85d128089765b008a38560125a40
灘　梨菜 <nada_rina@nadarina-no-MacBook-Air.local>
2016-10-23 16:39:45 +0900

    not use factory girls

--- spec/models/user_detail_spec.rb
+++ spec/models/user_detail_spec.rb
@@ -2,11 +2,12 @@ require 'rails_helper'
 
 describe UserDetail do
   # describe '#create' do
-    it "is invalid without a firstname"
-
-    it "is invalid without a lastname"
-
-    it "is invalid without a place"
-
-
+    it "is invalid without a firstname, lastname, place, postalcode" do
+      user_detail = UserDetail.new(
+        firstname: "Rina",
+        lastname: "Nada",
+        place: "toyama",
+        postalcode: "0000000")
+      expect(user_detail).to be_valid
+    end
 end
